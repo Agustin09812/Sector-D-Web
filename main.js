0a03456592db46820f0e8709d47a956e0e68dc7b
Agustin09812 <agustinivanlago@gmail.com>
2023-12-21 17:53:08 -0300

    update #6 gallery added

--- main.js
+++ main.js
@@ -1,42 +1,50 @@
-$(document).ready(function () {
-  let navbar = $('.navbar')
+document.addEventListener('DOMContentLoaded', () => {
 
-  let navbarHeight = navbar.height()
+  $(document).ready(function () {
+    let navbar = $('.navbar')
 
-  $(window).scroll(function () {
-    let scroll = $(window).scrollTop()
+    let navbarHeight = navbar.height()
 
-    if (scroll > navbarHeight) {
-      navbar.addClass('navbar-scrolled')
-    } else {
-      navbar.removeClass('navbar-scrolled')
-    }
-  })
-})
+    $(window).scroll(function () {
+      let scroll = $(window).scrollTop()
 
-function changeActive(element) {
-  let navLinks = document.querySelectorAll('.navbar-nav .nav-link')
-  navLinks.forEach(function (link) {
-    link.classList.remove('active')
+      if (scroll > navbarHeight) {
+        navbar.addClass('navbar-scrolled')
+      } else {
+        navbar.removeClass('navbar-scrolled')
+      }
+    })
   })
 
-  element.classList.add('active')
-}
+  function changeActive(element) {
+    let navLinks = document.querySelectorAll('.navbar-nav .nav-link')
+    navLinks.forEach(function (link) {
+      link.classList.remove('active')
+    })
 
-window.addEventListener('resize', function () {
-  let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-  let navbarNavList = document.getElementById('navbarNavList')
-  let navItems = navbarNavList.querySelectorAll('li')
-  navItems.forEach(function (item) {
-    if (windowWidth < 992) { // Ajustar el ancho en el que deseas aplicar el mb-3
-      item.classList.add('mb-3')
-    } else {
-      item.classList.remove('mb-3')
-    }
-  })
-  if (windowWidth < 992) { // Ajustar el ancho en el que deseas aplicar el mt-4
-    navbarNavList.classList.add('mt-4')
-  } else {
-    navbarNavList.classList.remove('mt-4')
+    element.classList.add('active')
   }
+
+  window.addEventListener('resize', function () {
+    let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+    let navbarNavList = document.getElementById('navbarNavList')
+    let navItems = navbarNavList.querySelectorAll('li')
+    navItems.forEach(function (item) {
+      if (windowWidth < 992) { // Ajustar el ancho en el que deseas aplicar el mb-3
+        item.classList.add('mb-3')
+      } else {
+        item.classList.remove('mb-3')
+      }
+    })
+    if (windowWidth < 992) { // Ajustar el ancho en el que deseas aplicar el mt-4
+      navbarNavList.classList.add('mt-4')
+    } else {
+      navbarNavList.classList.remove('mt-4')
+    }
+  })
+
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  })
+
 })
